15de5df35fef903ebf7c19c2fcccca6acaf92d70
N S Devika <nsdevika2000@gmail.com>
2026-07-09 14:28:20 +0530

    Add string formatting example in String_1.py

---
+++ Day_01/String_1.py
@@ -0,0 +1,13 @@
+str1='N S'
+str2='Devika'
+message=str1 +  '['+ str2 +'] is a coder'
+print(message)
+# Format function is used which contains placeholders
+msg=f'{str1}[{str2}] is a coder'
+print(msg)
+
+'''
+Output
+N S[Devika] is a coder
+N S[Devika] is a coder
+'''
