707c3510c89f0166cb593fc9953ec1e87c14bb59
N S Devika <nsdevika2000@gmail.com>
2026-07-09 14:25:29 +0530

    Add simple calculation functionality
    Implement a simple calculator that performs addition, subtraction, multiplication, and division.

---
+++ Day_01/Simple_calculation.py
@@ -0,0 +1,21 @@
+num1=int(input('Enter the first number'))
+num2=int(input('Enter the second number'))
+sum=num1+num2
+diff=num1-num2
+prod=num1*num2
+q=float(num1/num2)
+print('Sum=' + str(sum))
+print('Difference='+ str(diff))
+print('Product=' + str(prod))
+print('Quotient='+ str(q))
+
+'''
+Output
+
+Enter the first number2
+Enter the second number8
+Sum=10
+Difference=-6
+Product=16
+Quotient=0.25
+'''
